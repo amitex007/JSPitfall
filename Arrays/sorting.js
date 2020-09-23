b88a31abe44d63da1f0b86fe0837a2d9435d988a
Amitosh Anand <amitex007@gmail.com>
2020-09-23 16:32:08 +0530

    reference for sorting

--- Arrays/sorting.js
+++ Arrays/sorting.js
@@ -4,10 +4,11 @@
  * @Author: amitosh.anand 
  * @Date: 2020-09-23 16:10:28 
  * @Last Modified by: amitosh.anand
- * @Last Modified time: 2020-09-23 16:28:41
+ * @Last Modified time: 2020-09-23 16:31:23
  */
 
  // This code illustrates strange sorting order by JavaScript for non JS developers
+ // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 
  let a = [1,9,11,10,100000];
  a.sort();
@@ -35,7 +36,7 @@
  // Number Array: 
 
  function sortComparatorNumber(a,b){
-     
+
      if(typeof a=='number' && typeof b=='number') {
         return (a>b)? 1: (a==b)?0:-1;
      } else return 0;    
@@ -44,3 +45,5 @@
  let d = [1,9,11,10,100000];
  d.sort(sortComparatorNumber);
  console.log(d);
+
+ 
