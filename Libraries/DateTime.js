6a735766997eafeae62dbb2a233378fb0d18efbc
Amitosh Anand <amitex007@gmail.com>
2020-09-28 23:59:59 +0530

    date time added

---
+++ Libraries/DateTime.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: amitosh.anand 
+ * @Date: 2020-09-28 23:55:37 
+ * @Last Modified by: amitosh.anand
+ * @Last Modified time: 2020-09-28 23:58:59
+ */
+
+let today = new Date("2020-9-28");
+
+// Boring
+let date = today.getDate();
+console.log(date);
+
+// What happened
+let month = today.getMonth();
+console.log(month);
+// Months start from 0
+
+// Year start from 1900
+let year = today.getYear();
+console.log(year);
